72ee0259c71dddd6c5087c1f2cbcf0e3b7561d83
aayum <aayum@DESKTOP-K6I64UU>
2020-04-13 21:24:44 +0530

    commit bean scope

---
+++ springBeanScope/src/main/java/in/ibm/bean/CD.java
@@ -0,0 +1,36 @@
+package in.ibm.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CD {
+
+	private String cdID;
+	private String cdTitle;
+	private int cdYear;
+
+	public CD() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CD(String cdTitle, int cdYear) {
+		super();
+		this.cdTitle = cdTitle;
+		this.cdYear = cdYear;
+	}
+
+	public void getDetails() {
+		System.out.printf("CD_ID: %s CD_NAME: %s CD YEAR %d\n", getCdID(), getCdTitle(), getCdYear());
+	}
+	public void init() {
+		System.out.println("Init method called------");
+	}
+	
+	public void distory() {
+		
+		System.out.println("Destory method called -----------");
+	}
+
+}
